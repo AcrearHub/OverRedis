48dd9a1966b90f37dba60b7e41e9d52b22eddf4f
AcrearHub <lsp739305781@outlook.com>
2023-05-20 14:10:38 +0800

    test

---
+++ Flink/src/main/java/com/atguigu/WordCountBatch.java
@@ -0,0 +1,41 @@
+package com.atguigu;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.AggregateOperator;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.operators.UnsortedGrouping;
+import org.apache.flink.api.java.tuple.Tuple2;
+
+public class WordCountBatch {
+    public static void main(String[] args) throws Exception {
+        //1、创建Flink环境
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        //2、从数据源读数据
+        DataSource<String> dataSource = env.readTextFile("D:\\Atguigu\\05_Code\\OverRedis\\OverRedis\\FlinkInput\\word.txt");
+
+        //3、调用Flink API进行转换处理
+        dataSource
+                .flatMap((FlatMapFunction<String, Tuple2<String, Integer>>) (words, collector) -> {
+                    //根据空格来拆分
+                    String[] s1 = words.split(" ");
+                    for (String s2 : s1) {
+                        //调用of方法来将元素传入，返回Tuple类型值
+                        collector.collect(Tuple2.of(s2, 1));
+                    }
+                })
+                //根据报错类型，添加return方法来解决
+                .returns(new TypeHint<Tuple2<String, Integer>>() {})
+                /* groupby中：
+                KeySelector：任意类型都可以使用该方法，通过getKey方法来返回指定的key.
+                int：如果当前类型是Tuple,使用该方法，指定Tuple中的第几个元素作为key.
+                String：如果当前类型是P0J0(Bean),使用该方法，指定P0J0中的哪个属性作为水ey
+                */
+                .groupBy(0)
+        //4、汇总、输出结果
+                .sum(1)
+                .print();
+    }
+}
